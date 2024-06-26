31bc69342e919c65bf4fe6e6285ab35247d34c2f
lindseygulden <71043866+lindseygulden@users.noreply.github.com>
2024-06-26 08:33:10 -0400

    rooftop solar

--- projects/ccs/rooftop_solar_project.py
+++ projects/ccs/rooftop_solar_project.py
@@ -5,6 +5,8 @@ import logging
 from pathlib import PosixPath
 from typing import Tuple, Union
 
+import numpy_financial as npf
+
 from projects.ccs.project import Project
 
 logging.basicConfig(level=logging.INFO)
@@ -17,29 +19,43 @@ class RooftopSolarProject(Project):
         # initialize parent class __init__
         super().__init__(params)
         self.type = "rooftop_solar"
+
+        # details about this particular solar array
         self.region = self.config["region"]
         self.state = self.config["state"]
         self.tilt = self.config["tilt"]
         self.azimuth = self.config["azimuth"]
         self.system_size_kw = self.config["kw"]
-        self.installation_cost_usd = self.config["installation_cost_usd"]
+        self.kwh_per_yr = self.config["kwh_per_yr"]  # from PVwatts
 
-        # If yearly time series of tco2 sequestered is provided, use that as basis for project and project length
-        self.kwh_per_yr = self.config["kwh_per_yr"]
+        # details about carbon intensity and price of electricity in this location, cost of PV array install
+        self.installation_cost_usd = self.config["installation_cost_usd"]
         self.tco2_per_kwh = self.config["tco2_per_kwh"]
         self.usd_per_kwh = self.config["usd_per_kwh"]
 
+        # compute financials
         self.avg_discounted_unit_value_solar_usd_per_yr = (
-            self._compute_discounted_annual_avg_revenue()
+            self._avg_discounted_unit_cash_flow(
+                [self.kwh_per_yr] * self.project_length_yrs,
+                [self.usd_per_kwh] * self.project_length_yrs,
+            )
         )
         self.usd_per_yr = self._compute_usd_per_yr()
+
+        # tco2 totals and tco2/yr
+
         self.tco2_per_yr, self.total_tco2 = self._compute_tco2()
-        self.present_value_of_installation_cost = (
-            self._compute_discounted_annual_avg_cost()
+        self.pv_all_solar = npf.npv(
+            self.discount_rate - self.inflation_rate,
+            [self.usd_per_yr] * self.project_length_yrs,
         )
-        self.cost_usd_per_tco2 = self._compute_unit_value_per_tco2(
-            self.present_value_of_installation_cost
+        self.pv_cost = npf.npv(
+            self.discount_rate - self.inflation_rate,
+            [self.installation_cost_usd] + [0] * (self.project_length_yrs - 1),
         )
+
+        self.cost_usd_per_tco2 = self.pv_cost / self.total_tco2
+
         self.avg_discounted_unit_value_solar_usd_per_tco2 = (
             self._compute_unit_value_per_tco2(
                 self.avg_discounted_unit_value_solar_usd_per_yr
@@ -48,16 +64,6 @@ class RooftopSolarProject(Project):
 
         # once we've assigned all variables in the config file, delete the attribute
         delattr(self, "config")
-
-    def _compute_discounted_annual_avg_revenue(self) -> float:
-        """compute annual average discounted revenue of solar energy produced"""
-
-        # compute average value of solar production (per year)
-        discounted_annual_revenue = self._avg_discounted_unit_cash_flow(
-            [self.kwh_per_yr] * self.project_length_yrs,
-            [self.usd_per_kwh] * self.project_length_yrs,
-        )
-        return discounted_annual_revenue
 
     def _compute_usd_per_yr(self) -> float:
         """Computes nominal expenditure in today's USD for value of kwh per year"""
@@ -73,16 +79,6 @@ class RooftopSolarProject(Project):
         total_tco2 = tco2_per_year * self.project_length_yrs
 
         return (tco2_per_year, total_tco2)
-
-    def _compute_discounted_annual_avg_cost(self) -> float:
-        """compute annual-average discounted cost of solar array"""
-        # TODO eliminate hard coding for spending (currently all spending is in year 1)
-
-        discounted_unit_cost = self._avg_discounted_unit_cash_flow(
-            [self.installation_cost_usd] + [0] * (self.project_length_yrs - 1),
-            [1] + [0] * (self.project_length_yrs - 1),
-        )
-        return discounted_unit_cost
 
     def _compute_price_of_tco2_removal(self) -> float:
         """Computes the total cost of tco2 removal"""
