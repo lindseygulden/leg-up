dffb71417d10cfdd5565943c6d6014a0ba0756c3
Lindsey Gulden <lindsey.gulden@icloud.com>
2024-09-21 08:05:28 -0400

    saving

--- projects/lobbying/lda_query.py
+++ projects/lobbying/lda_query.py
@@ -24,7 +24,8 @@ def assemble_issue_search_string(
     term_list_dict = yaml_to_dict(term_list_path)
     law_list_dict = yaml_to_dict(law_list_path)
     search_string = "OR".join(
-        term_list_dict["search_term_list"]  # + law_list_dict["mostly_ccs_provisions"]
+        term_list_dict["search_term_list"]  # +
+        # law_list_dict["mostly_ccs_provisions"]
     )
     # law_list_dict["contains_ccs_provisions"]
     return search_string
@@ -290,6 +291,7 @@ def query_lda(config: Union[str, PosixPath], output_dir: Union[str, PosixPath]):
     filing_id = 0  # initialize unique id for filing documents
     row_list = []  # each row holds info for one lobbying activity
     lobby_list = []  # initialize holder for lobbyist info
+    # which_chunk = 11
     # which_chunk = 146  # for laws
     for page in range(1, n_pages + 1):
         # initialize holders for upcoming subset's information ('chunk')
