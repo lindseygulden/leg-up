f143d9ab2ad9f01a42976b72503fa1362fc3b3f6
lindseygulden <71043866+lindseygulden@users.noreply.github.com>
2025-01-22 17:28:16 -0500

    Add files via upload

---
+++ projects/lobbying/config/postproc_utils.py
@@ -0,0 +1,206 @@
+"""utility functions to compile and postprocess files read in from LDA API
+called by both postproc_utils.py and compile_results.py
+"""
+
+import datetime as dt
+import re
+from itertools import compress
+from typing import Dict, List, Tuple, Union
+
+import pandas as pd
+from cleanco import basename
+
+from utils.io import yaml_to_dict
+
+
+def terms_present(phrase, term_list, find_any=True):
+    """utility function to see if terms in terms_list are present in a given phrase
+    Args:
+        phrase: phrase to be searched
+        term_list: list of strings to be searched for within phrase
+        find_any: boolean -- true if function should return true if any of the terms are in the phrase;
+            false if the function should only return true if all terms are present
+    Returns:
+        int: 1 if one or more of the terms are present in phrase, 0 otherwise
+    """
+
+    if not isinstance(phrase, str):
+        raise TypeError("phrase must be a string")
+    if not isinstance(term_list, list):
+        raise TypeError(
+            "term_list must be a list of strings (even if it is a list of length 1)"
+        )
+    if phrase is None:
+        return 0
+    n_present = 0
+
+    for term in term_list:
+        if isinstance(term, list):
+            if terms_present(phrase, term, find_any=False):
+                n_present += 1
+        elif term.lower() in phrase.lower():
+            n_present += 1
+    if find_any is False:  # find all
+        if len(term_list) == n_present:
+            return 1
+        return 0
+    return n_present
+
+
+def get_list_govt_entities(entity_endpoint: str, session: object):
+    """Queries constants endpoint to get a standardized list of government entities"""
+    govt_entities = session.get(entity_endpoint, timeout=60)
+    entity_df = pd.DataFrame(govt_entities.json())
+    entities = sorted([x.lower() for x in list(entity_df["name"])])
+    return entities
+
+
+def substitute(
+    x: Union[str, List[str]],
+    use_basename: bool = False,
+    re_types: str = r"[^\w\s]",
+    replace_str: str = "",
+):
+    """wrapper function for regular expression substitute funciton, linked with basename lib"""
+    # use basename for company names
+
+    if (not isinstance(x, str)) and (not isinstance(x, list)):
+        return replace_str
+    if isinstance(x, list):
+        return [
+            substitute(
+                xx,
+                use_basename=use_basename,
+                re_types=re_types,
+                replace_str=replace_str,
+            )
+            for xx in x
+        ]
+
+    if use_basename:
+        return basename(re.sub(re_types, replace_str, x))
+    # don't use basename for general strings
+    return re.sub(re_types, replace_str, x)  # .rstrip().lstrip()
+
+
+def parse_client_names(
+    input_df, config, client_rename_col="client_rename"
+) -> Tuple[pd.DataFrame, Dict[str, str]]:
+    """bespoke function for parsing organization names from LDA queries
+    Args:
+        input_df: input pandas dataframe (read raw from files saved after api access)
+        config: configuration dictionary containing details on how to handle names
+        client_rename_column: name for new column in dataframe that is returned
+    Returns:
+        output_df: processed column with renamed clients and removed organziations
+        client_name_rename_dict: dictionary used for renaming (contains original and new names)
+    """
+    output_df = input_df.copy(deep=True)
+
+    starting_client_names = sorted(list(output_df.client_name.unique()))
+    client_names = sorted(list(output_df.client_name.unique()))
+    client_names = [
+        substitute(company_name, use_basename=True) for company_name in client_names
+    ]
+
+    # take rightmost component of client name
+    for term in config["take_terms_to_the_right_of_these_words"]:
+        client_names = [x.split(term)[-1] for x in client_names]
+    # take leftmost component of client name
+    for term in config["take_terms_to_the_left_of_these_words"]:
+        client_names = [x.split(term)[0] for x in client_names]
+    # remove words
+    for n in config["remove_these_phrases"]:
+        client_names = [x.replace(n, "") for x in client_names]
+    # get rid of double spaces
+    client_names = [x.replace("  ", " ") for x in client_names]
+    # trim spaces on ends of names
+    client_names = [x.rstrip().lstrip() for x in client_names]
+
+    # extract shorter, well-known names from longer names
+    for co in config["use_these_name_subsets_for_organiztions"]:
+        client_names = [co if co in x else x for x in client_names]
+
+    # bespoke replacements and handling of mergers
+    for key, value in config["replace_names_on_left_with_names_on_right"].items():
+        client_names = [x.replace(key, value) for x in client_names]
+
+    # make a renaming dictionary
+    client_name_rename_dict = dict(zip(starting_client_names, client_names))
+
+    # add the 'remove' companies to the rename dictionary
+    remove_companies = config["remove_companies_containing_these_terms"]
+    for original_name, new_name in client_name_rename_dict.items():
+        if terms_present(
+            new_name,
+            remove_companies,
+            find_any=True,
+        ):
+            client_name_rename_dict[original_name] = "remove"
+
+    # make new column with renames
+    output_df[client_rename_col] = [
+        client_name_rename_dict[x] for x in output_df.client_name
+    ]
+    output_df = output_df.loc[output_df[client_rename_col] != "remove"]
+
+    return output_df, client_name_rename_dict
+
+
+def get_smarties(
+    row: Union[pd.Series, List[Union[bool, int]]], names: List[str]
+) -> List[str]:
+    """opposite of 'get dummies': returns list of all bool columns for which value is True"""
+    if isinstance(row, pd.Series):
+        if len(row) == 0:
+            return []
+
+        return list(compress(names, row[names].values.tolist()))
+    if isinstance(row, list):
+        return list(compress(names, row))
+    raise TypeError("get_smarties argument 'row' must be a Pandas Series or a list")
+
+
+def get_latest_filings(
+    df: pd.DataFrame, groupby_cols: List[str], date_col="filing_dt_posted"
+):
+    """gets only the latest filing for a given lobbying firm, client, and quarter
+    Args:
+        df: pandas dataframe containing all query results, some of which are duplicative
+        groupby_cols: list of column names for identifying the most-recent filing for an activity
+        date_col: string name of date column for identifying most recent actiity
+    Returns:
+        df: culled so only the most-recent filing for each client/lobbying firm/quarter remains
+    """
+    if df[date_col].dtype == str:
+        df[date_col] = [dt.datetime.fromisoformat(d) for d in df[date_col]]
+
+    df.sort_values(by=date_col, ascending=False, inplace=True)
+
+    df = df.groupby(groupby_cols).first().reset_index()
+    return df
+
+
+def invert_sector_dict(sectors_path: str) -> Dict[str, str]:
+    """reads in the sector assignment yaml to dict; inverts dict s.t. each company is a key
+    Args:
+        sectors_path: path to sector-assignments yaml file
+    Returns:
+        dictionary mapping each company name (key) to its granular sector (value)
+    """
+    if isinstance(sectors_path, list):
+        sector_assignments = {}
+        for p in sectors_path:
+            sector_assignments = sector_assignments | yaml_to_dict(p)
+    else:
+        sector_assignments = yaml_to_dict(sectors_path)
+    all_companies = []
+    for _, value in sector_assignments.items():
+        all_companies = all_companies + value
+
+    company_sector_dict = {}
+    for k, vv in sector_assignments.items():
+        for v in vv:
+            company_sector_dict = company_sector_dict | {v: k}
+
+    return company_sector_dict
---
+++ projects/lobbying/config/postprocess.py
@@ -0,0 +1,514 @@
+# pylint: disable=use-a-generator
+"""command-line tool/script to postprocess compiled lobbying activity files"""
+
+import logging
+from pathlib import PosixPath
+from typing import List, Tuple, Union
+
+import click
+import numpy as np
+import pandas as pd
+from asteval import Interpreter
+
+from projects.lobbying.postproc_utils import (
+    invert_sector_dict,
+    substitute,
+    terms_present,
+)
+from utils.io import yaml_to_dict
+
+logging.basicConfig(level=logging.INFO)
+
+
+def quarter_to_decimal(quart: str) -> float:
+    """converts string quarter to decimal fraction of year"""
+    if quart == "second_quarter":
+        return 135 / 365.25
+    if quart == "third_quarter":
+        return 227 / 365.25
+    if quart == "first_quarter":
+        return 46 / 365.25
+    if quart == "fourth_quarter":
+        return 319 / 365.25
+    if quart == "year_end":
+        return 274 / 265.25
+    if quart == "mid_year":
+        return 91 / 365.25
+    raise ValueError(f"{quart} quarter string is not one of accepted values.")
+
+
+def adjust_company_names(df: pd.DataFrame, config_info: dict):
+    """Fills nans, replaces company names according to replacmeents specified in yaml"""
+
+    activity_df = df.copy()
+    logging.info(" ADJUSTING THE COMPANY NAMES!")
+
+    activity_df[config_info["clean_client_description_col"]] = activity_df[
+        config_info["clean_client_description_col"]
+    ].fillna("")
+    activity_df[config_info["company_rename_col"]] = activity_df[
+        config_info["company_rename_col"]
+    ].fillna("")
+
+    replace_dict = yaml_to_dict(config_info["company_name_replacements"])
+
+    activity_df[config_info["company_rename_col"]] = [
+        replace_dict[x] if x in list(replace_dict.keys()) else r
+        for x, r in zip(
+            activity_df["client_name"], activity_df[config_info["company_rename_col"]]
+        )
+    ]
+
+    return activity_df
+
+
+def get_term_lists(list_path: str, list_key: str) -> Tuple[List[str], List[List[str]]]:
+    """Gets and processes term lists describing CCS"""
+
+    search_terms = yaml_to_dict(list_path)[list_key]
+
+    terms = []
+    for term in search_terms:
+        if "," in term:
+            terms.append(term.replace('"', "").split(","))
+        else:
+            terms.append([term.replace('"', "")])
+
+    single_terms = []
+    multiple_terms = []
+    for x in terms:
+        if len(x) == 1:
+            single_terms.append(x[0])
+        else:
+            multiple_terms.append(x)
+
+    return (single_terms, multiple_terms)
+
+
+def assign_sectors(df: pd.DataFrame, config_info: dict):
+    """Given info in yamls, assigns companies to sectors and lumped sectors; adds new columns to df"""
+    inverted_sector_dict = invert_sector_dict(config_info["company_sector_assignments"])
+    lumped_sector_dict = yaml_to_dict(config_info["path_to_lumped_sector_info"])
+
+    df[config_info["company_rename_col"]] = df[
+        config_info["company_rename_col"]
+    ].fillna(" ")
+
+    orgs = df[config_info["company_rename_col"]].unique()
+    for org in orgs:
+        if org not in inverted_sector_dict.keys():
+            print(org)
+    df["sector"] = [
+        inverted_sector_dict[x] for x in df[config_info["company_rename_col"]]
+    ]
+    df["lumped_sector"] = [lumped_sector_dict["lightly_lumped"][s] for s in df.sector]
+    df["very_lumped_sector"] = [lumped_sector_dict["very_lumped"][s] for s in df.sector]
+    df["uber_lumped_sector"] = [lumped_sector_dict["uber_lumped"][s] for s in df.sector]
+    return df
+
+
+def find_description(
+    df: pd.DataFrame,
+    descriptor: str,
+    filepath: str,
+    return_count: bool = True,
+):
+    """For each lobbying activity (row), determine whether any of a set of terms are in description"""
+    # are terms consistent with descriptor in the lobbying description? (intermediate variables)
+
+    terms = yaml_to_dict(filepath)[descriptor]
+    terms = substitute(terms, use_basename=False)
+
+    df[f"count_{descriptor}"] = [terms_present(x, terms) for x in df.clean_description]
+
+    # if description of lobbying activity contains either terms from the single-term list
+    # or from the multi-term list, indicate that the activity contains description
+    df[f"{descriptor}"] = df[f"count_{descriptor}"].astype(bool).astype(int)
+    if return_count:
+        return df
+    df.drop(f"count_{descriptor}", axis=1, inplace=True)
+    return df
+
+
+def find_bill_numbers_for_congress(
+    df: pd.DataFrame, postproc_specs: str, id_str: str, min_chars: int = 7
+):
+    """identify lobbying activities (rows) that mention CCS-focused bills for a given congress"""
+    # get dictionary with congress number/bill number for CCS bills
+
+    bill_numbers = yaml_to_dict(postproc_specs)[id_str]
+    # identify activities that reference specific bill numbers
+    return [
+        (
+            1
+            if (
+                terms_present(
+                    d,
+                    [
+                        x + " " if len(x) < min_chars else x
+                        for x in bill_numbers[which_congress]
+                    ],
+                )
+                | terms_present(
+                    d,
+                    [
+                        (
+                            x.replace(" ", "") + " "
+                            if len(x) < min_chars
+                            else x.replace(" ", "")
+                        )
+                        for x in bill_numbers[which_congress]
+                    ],
+                )
+                | terms_present(
+                    d,
+                    [
+                        (
+                            x.replace("SB ", "S") + " "
+                            if len(x) < min_chars
+                            else x.replace("SB ", "S")
+                        )
+                        for x in bill_numbers[which_congress]
+                    ],
+                )
+                | terms_present(
+                    d,
+                    [
+                        (
+                            x.replace("SB ", "S ") + " "
+                            if len(x) < min_chars
+                            else x.replace("SB ", "S ")
+                        )
+                        for x in bill_numbers[which_congress]
+                    ],
+                )
+            )
+            else 0
+        )
+        for d, which_congress in zip(df.clean_description, df.which_congress)
+    ]
+
+
+def identify_lobbying_activities(df: pd.DataFrame, config_info: dict):
+    """Use terms, law names, & sectors to identify very-likely, likely, & potentially
+    relevant lobbying activities
+    Args:
+        df: pandas dataframe containing lobbying activity information
+        config_info: dictionary read in from configuration yaml with paths to data
+    Returns:
+        df: updated pandas dataframe with binary columns and term-count columns indicating
+        related lobbying activity
+    """
+
+    topic = config_info["topic_description"]
+
+    # find descriptions of subsets of terms for which also want a term count
+    # identify activities that are explicitly the topic
+    for t in [
+        "contains_description",
+        "clean_h2_description",
+        f"bills_with_some_{topic}",
+    ]:
+        df = find_description(
+            df, t, config_info["postproc_specs_path"], return_count=True
+        )
+
+    # find descriptions of subsets of terms for which we don't want a term count
+    for t in [
+        f"{topic}_bills",
+        f"bills_with_{topic}_terms",
+        f"not_{topic}",
+        "h2_mention",
+        f"terms_consistent_with_{topic}",
+        f"terms_could_be_{topic}",
+    ]:
+        df = find_description(
+            df, t, config_info["postproc_specs_path"], return_count=False
+        )
+
+    # identify activities that reference bills/laws with CCS provisions; return count
+    df[f"{topic}_bills_number_only"] = find_bill_numbers_for_congress(
+        df, config_info["postproc_specs_path"], "congress_bill_nos"
+    )
+
+    postproc_dict = yaml_to_dict(config_info["postproc_specs_path"])
+    # handle 'nebulous' hydrogen terms (e.g., "low-carbon hydrogen", NOT terms such as "clean hydrogen",
+    # which is definitely CCS, and which is handled as a CCS term, above)
+
+    df["h2_mention_core_ff"] = [
+        1 if ((h == 1) and (s in postproc_dict["core_industry_sectors"])) else 0
+        for h, s in zip(df.h2_mention, df.sector)
+    ]
+    df["h2_mention_ff_adjacent"] = [
+        1 if ((h == 1) and (s in postproc_dict["industry_adjacent_sectors"])) else 0
+        for h, s in zip(df.h2_mention, df.sector)
+    ]
+    # identify a few 'always CCS' and 'always not CCS' sectors
+    for sector in [f"{topic}", "clean hydrogen", "green hydrogen"]:
+        # is this a company dedicated to CCS tech and operations?
+        df[f"{sector.replace(' ','_')}_company"] = [
+            1 if x == sector else 0 for x in df.sector
+        ]
+
+    # find those that, b/c of industry, 'probably on topic' lobbying activities are almost
+    # certainly lobbying on the topic.
+    # HACK omit findings of the 'low carbon economy' act, which has the term 'low carbon', but
+    # didn't deal with ccs
+    # TODO refactor this
+    df["low_carbon_economy_act"] = [
+        1 if terms_present(x, ["low carbon economy", "lowcarbon economy"]) else 0
+        for x in df.clean_description
+    ]
+    df[f"{topic}_because_of_who_says_it"] = [
+        (
+            1
+            if (
+                (
+                    probably
+                    and (not lca)
+                    and (sector in postproc_dict["industry_adjacent_sectors"])
+                )
+                | (
+                    bill_with_terms
+                    and (sector in postproc_dict["industry_adjacent_sectors"])
+                )
+            )
+            else 0
+        )
+        for probably, bill_with_terms, sector, lca in zip(
+            df[f"terms_consistent_with_{topic}"],
+            df[f"bills_with_{topic}_terms"],
+            df.sector,
+            df.low_carbon_economy_act,
+        )
+    ]
+
+    # classify a lobbying activity as very likely on topic if it either contains a description,
+    # is a company that is focused solely or primarily on the topic (e.g., CCS)
+    # clean hydrogen company, mentions relevant bills, and does not contain a 'not on topic' term
+    df[f"definitely_{topic}"] = [
+        1 if ((d + +b + bn + c + h + hff) > 0) & (n == 0) else 0
+        for d, b, bn, c, h, hff, n in zip(
+            df.contains_description,
+            df[f"{topic}_bills"],
+            df[f"{topic}_bills_number_only"],
+            df[f"{topic}_company"],
+            df.clean_hydrogen_company,
+            df.h2_mention_core_ff,
+            df[f"not_{topic}"],
+        )
+    ]
+
+    #
+    df[f"very_likely_{topic}"] = [
+        1 if ((d + h + w) > 0) & (n == 0) else 0
+        for d, h, w, n in zip(
+            df[f"definitely_{topic}"],
+            df.h2_mention_ff_adjacent,
+            df[f"{topic}_because_of_who_says_it"],
+            df[f"not_{topic}"],
+        )
+    ]
+
+    # likely on topic are all the 'very likely' activities plus the 'core industry sector'
+    # organizations paired with 'probably' activity descriptions
+    df[f"likely_{topic}"] = [
+        1 if (((vl + lean + law) > 0) & (n == 0)) else 0
+        for vl, n, lean, law in zip(
+            df[f"very_likely_{topic}"],
+            df[f"not_{topic}"],
+            df[f"terms_consistent_with_{topic}"],  # probably ccs, but not definitely
+            df[f"bills_with_{topic}_terms"],
+        )
+    ]
+    # identify the activities that could be on topic because of relevant terms and industry
+    df[f"could_be_{topic}"] = [
+        (
+            1
+            if (
+                maybe
+                and (not lca)
+                and (sector in postproc_dict["industry_adjacent_sectors"])
+            )
+            else 0
+        )
+        for maybe, sector, lca in zip(
+            df[f"terms_could_be_{topic}"], df.sector, df.low_carbon_economy_act
+        )
+    ]
+    df[f"potentially_{topic}"] = [
+        1 if ((cb + l) > 0) & (n == 0) else 0
+        for cb, l, n in zip(
+            df[f"could_be_{topic}"],
+            df[f"likely_{topic}"],
+            df[f"not_{topic}"],
+        )
+    ]
+
+    return df
+
+
+def apportion_filing_dollars_to_activities(df: pd.DataFrame):
+    """apportion total lobbying dollars spent (on filing) to individual lobbying activities"""
+
+    # basic method: aportion using total number of activities
+    df["activity_apportioned_usd"] = [
+        usd / number_lobbying
+        for usd, number_lobbying in zip(
+            df.dollars_spent_lobbying, df.total_number_lobbying_activities
+        )
+    ]
+    # apportion total filing dollars to activities using the fraction of the total number
+    # lobbyists on the filing assigned to this activity
+    df["lobbying_activity_usd"] = [
+        (
+            usd * (n_activity_lobbyists / total_lobbyists)
+            if total_lobbyists > 0
+            else activity_apportioned
+        )
+        for usd, n_activity_lobbyists, total_lobbyists, activity_apportioned in zip(
+            df.dollars_spent_lobbying,
+            df.n_lobbyists_for_activity,
+            df.total_number_of_lobbyists_on_filing,
+            df.activity_apportioned_usd,
+        )
+    ]
+
+    return df
+
+
+def subset_to_topic_only(df: pd.DataFrame, config_info: dict):
+    """Get rid of the activities/rows that are not likely dealing with the topic of interest"""
+    topic = config_info["topic_description"]
+    cleaned_df = df.loc[
+        (df.lumped_sector != config_info["remove_sector_name"])
+        & (
+            (df[f"very_likely_{topic}"] == 1)
+            | (df[f"likely_{topic}"] == 1)
+            | (df[f"potentially_{topic}"] == 1)
+        )
+    ].copy(deep=True)
+
+    return cleaned_df
+
+
+def expand_and_tabulate_entities(
+    df: pd.DataFrame, config: str
+) -> Tuple[pd.DataFrame, List[str]]:
+    """Use entity string to expand and tabulate counts of lobbying activities"""
+    entities = yaml_to_dict(config)["tabulate_contacts_for_entities"]
+
+    # safe eval
+    aeval = Interpreter()
+
+    # expand the json string of the entity list
+    df["entity_expanded"] = [aeval(x) for x in df["entities"]]
+    df["n_entities_lobbied"] = [len(x) for x in df.entity_expanded]
+    df["legistlative_entities_lobbied"] = [
+        np.sum([("congress" in x) | ("house" in x) | ("senate" in x) for x in xx])
+        for xx in df.entity_expanded
+    ]
+    df["executive_entities_lobbied"] = [
+        t - l for t, l in zip(df.n_entities_lobbied, df.legistlative_entities_lobbied)
+    ]
+    # make binary variables for each govt. entity
+    for entity in entities:
+        df[entity] = [
+            1 if entity in entity_list else 0 for entity_list in df.entity_expanded
+        ]
+
+    return df, entities
+
+
+def add_political_party(df: pd.DataFrame, config_info: dict):
+    """identify party in control of house, senate, and white house at time of each activity"""
+    # get mapping for each congress and each branch of govt
+    party_dict = yaml_to_dict(config_info["political_party_congress_path"])
+
+    for b in ["senate", "house", "white_house"]:
+        df["party_controlling_" + b] = [party_dict[b][c] for c in df.which_congress]
+
+    return df
+
+
+@click.command()
+@click.option(
+    "--config",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=True,
+)
+@click.option(
+    "--input_file", type=click.Path(file_okay=True, dir_okay=False), required=True
+)
+@click.option(
+    "--output_file", type=click.Path(file_okay=True, dir_okay=False), required=True
+)
+def postprocess(
+    config: Union[str, PosixPath],
+    input_file: Union[str, PosixPath],
+    output_file: Union[str, PosixPath],
+):
+    """Reads compiled ccs LDA API query outputs and identifies CCS lobbying activities"""
+    config_info = yaml_to_dict(config)
+
+    topic = config_info["topic_description"]
+
+    all_df = pd.read_csv(input_file)
+
+    logging.info(" >>> Replacing company names")
+    all_df = adjust_company_names(all_df, config_info)
+
+    logging.info(" >>> Assigning companies to sectors")
+    all_df = assign_sectors(all_df.copy(deep=True), config_info)
+
+    logging.info(" >>> Classifying lobbying activities")
+    # get rid of nans in lobbying activity description
+    all_df.clean_description = all_df.clean_description.fillna(" ")
+    all_df = identify_lobbying_activities(all_df, config_info)
+
+    if config_info["subset_to_topic_only"]:
+        logging.info(" >>> Subsetting lobbying activities")
+        activity_df = subset_to_topic_only(all_df, config_info)
+    else:
+        activity_df = all_df
+
+    logging.info(" >>> Apportioning dollars to individual lobbying activities")
+    activity_df = apportion_filing_dollars_to_activities(activity_df)
+
+    activity_df, entities = expand_and_tabulate_entities(
+        activity_df, config_info["postproc_specs_path"]
+    )
+
+    activity_df = add_political_party(activity_df, config_info)
+    logging.info(" >>> Writing out data")
+    postproc_dict = yaml_to_dict(config_info["postproc_specs_path"])
+    if "rename_columns" in postproc_dict:
+        activity_df.rename(columns=postproc_dict["rename_columns"], inplace=True)
+
+    activity_df = activity_df[
+        postproc_dict["subset_and_order_of_writeout_columns"] + entities
+    ]
+
+    # order values such that someone who is reading the output first sees all the 'definitely ccs' ones
+    activity_df = activity_df.sort_values(
+        by=[
+            f"definitely_{topic}",
+            f"very_likely_{topic}",
+            f"likely_{topic}",
+            f"potentially_{topic}",
+            "sector",
+            "organization",
+            "filing_year",
+        ],
+        ascending=False,
+    )
+    activity_df.to_csv(output_file, index=False)
+
+    logging.info(
+        " ----- Postprocessed file written to %s ",
+        str(output_file),
+    )
+
+
+if __name__ == "__main__":
+    postprocess()
